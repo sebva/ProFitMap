19f37372d407652426a663b059f48f2eaebaf8c2
Sébastien Vaucher <seb.vaucher@gmail.com>
2014-01-16 08:39:21 +0000

    Dropbox photo view rotation
    git-svn-id: https://svn.isic.lan/13dlm-ta214@73 ef76b29e-2656-4f73-ac72-ae90624dffdc

--- ProFitMap/src/ch/hearc/profitmap/gui/DropboxPhotoViewerActivity.java
+++ ProFitMap/src/ch/hearc/profitmap/gui/DropboxPhotoViewerActivity.java
@@ -2,6 +2,7 @@ package ch.hearc.profitmap.gui;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
 import ch.hearc.profitmap.R;
@@ -14,13 +15,21 @@ public class DropboxPhotoViewerActivity extends Activity
 {
 	private DropboxImageView imageView;
 	private PhotoViewAttacher attacher;
+	private DbxPath path;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_dropbox_photo_viewer);
+		path = new DbxPath(getIntent().getStringExtra("path"));
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		
+		setupImageView();
+	}
+
+	private void setupImageView()
+	{
+		setContentView(R.layout.activity_dropbox_photo_viewer);
 		imageView = (DropboxImageView) findViewById(R.id.image_view);
 		attacher = new PhotoViewAttacher(imageView);
 		imageView.setListener(new ImageLoadedListener()
@@ -32,10 +41,14 @@ public class DropboxPhotoViewerActivity extends Activity
 			}
 		});
 		
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		DbxPath path = new DbxPath(getIntent().getStringExtra("path"));
 		imageView.loadImageFromDropbox(path, DropboxManager.getInstance().getFilesystem(), true);
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) 
+	{
+		super.onConfigurationChanged(newConfig);
+		setupImageView();
 	}
 	
 	@Override
